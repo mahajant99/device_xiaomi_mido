6443c9ab0b6af0daaaa77a5f321e86eed1e21927
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    mido: Update qcom sepolicy include
    Change-Id: I7167575e82eb8c5de2d822643261757edda73a07
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,7 +206,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 VENDOR_SECURITY_PATCH := 2020-08-01
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
@@ -238,4 +238,3 @@ BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/mido/BoardConfigVendor.mk
-
